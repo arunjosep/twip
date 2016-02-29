2ba8b855b5b2a94383fb28184408e5a5c8f06fd8
Arun Jose <arunjose_p@yahoo.co.in>
2016-03-01 01:05:06 +0530

    Initial commit

---
+++ src/main/java/com/arjose/App.java
@@ -0,0 +1,13 @@
+package com.arjose;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
