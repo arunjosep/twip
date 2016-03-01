f8abe10019ebcc8a9c04e2df233fcd61b41885b2
Arun Jose <arunjose_p@yahoo.co.in>
2016-03-02 00:47:31 +0530

    modified:   src/main/java/com/arjose/twip/spouts/PanchayathSpout.java

--- src/main/java/com/arjose/twip/spouts/PanchayathSpout.java
+++ src/main/java/com/arjose/twip/spouts/PanchayathSpout.java
@@ -1,35 +1,35 @@
 package com.arjose.twip.spouts;
 
 import java.util.Map;
-
 import backtype.storm.spout.SpoutOutputCollector;
 import backtype.storm.task.TopologyContext;
 import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.topology.base.BaseRichSpout;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Values;
+import backtype.storm.utils.Utils;
 
 public class PanchayathSpout extends BaseRichSpout {
+	SpoutOutputCollector collector;
+	int num;
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 	@Override
 	public void open(Map conf, TopologyContext context, SpoutOutputCollector collector) {
-		// TODO Auto-generated method stub
-
+		this.collector = collector;
+		num=0;
 	}
 
 	@Override
 	public void nextTuple() {
-		// TODO Auto-generated method stub
-
+	    Utils.sleep(100);
+	    collector.emit(new Values("tuple "+num++));
 	}
 
 	@Override
 	public void declareOutputFields(OutputFieldsDeclarer declarer) {
-		// TODO Auto-generated method stub
-
+		declarer.declare(new Fields("tweet"));
 	}
 
 }
