730093cf0fbe049987298005dd6a843f8f38c40c
Arun Jose <arunjose_p@yahoo.co.in>
2016-03-01 16:06:10 +0530

    new file:   src/main/java/com/arjose/twip/spouts/PanchayathSpout.java 	new file:   target/.gitignore

---
+++ src/main/java/com/arjose/twip/spouts/PanchayathSpout.java
@@ -0,0 +1,35 @@
+package twip.spouts;
+
+import java.util.Map;
+
+import backtype.storm.spout.SpoutOutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseRichSpout;
+
+public class PanchayathSpout extends BaseRichSpout {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void open(Map conf, TopologyContext context, SpoutOutputCollector collector) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void nextTuple() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void declareOutputFields(OutputFieldsDeclarer declarer) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
